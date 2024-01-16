7e7085833b0c0560208e2bbbe7038a3feb139629
Mauriceac <maurice.alvarado@teck.com>
2024-01-16 16:24:20 -0300

    gemini with slack and groundx

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { App } = require('@slack/bolt');
 const { Groundx } = require('groundx-typescript-sdk');
-const OpenAI = require('openai');
+// const OpenAI = require('openai');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 require('dotenv').config();
 
@@ -14,52 +15,49 @@ const app = new App({
 const groundx = new Groundx({
     apiKey: process.env.GROUNDX_API_KEY,
 })
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-});
 
 app.event('app_mention', async ({ event, context }) => {
     
     try {
         // Send user's message to GroundX API
-        console.log('Received message:', event.text);
-        const message = event.text.replace(/<@U[A-Z0-9]+>/, '');
+    console.log('Received message:', event);
+    const message = event.text.replace(/<@U[A-Z0-9]+>/, '');
+
+    const groundXResponse = await groundx.search.content({
+        id: 7120,
+        query: message,
+        //n: 5
+    });
+
+    llmText = groundXResponse.data.search.text
     
-        const groundXResponse = await groundx.search.content({
-            id: 7120, //change this number with the id of the bucket or project you want to search through
-            query: message,
-            n: 5 //number of results to return, default is 20; ChatGPT has a limit of "tokens" per request, so we're limiting the number of results to 5 just in case.
+    // initiate Gemini
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+    // Indicate model
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+    // Indicate prompt containing instructions, content to work with, and user message
+    const prompt = `You're a cheerful chatbot for an online course. Greet the student using their ID: <@${event.user}>. Use the content provided below to generate a response to this student's message. Use emojis in your response. Indicate the sources you've been given, if any. If the provided content is inadequate to respond, answer 'I don't have enough information to answer your question. Please contact <@U068QGDA8E5>'.
+    ===
+    student message: ${message}
+    ===
+    Provided content: ${llmText}
+    note: Challenge Course and Phase 1 are different names used to indicate the first part of the scholarship program.
+    ===`;
+  
+    const result = await model.generateContent(prompt);
+    const response = result.response;
+    const text = response.text();
+    console.log(text);
+
+    // Send LLM response back to the user in the Slack thread
+    
+        await app.client.chat.postMessage({
+            token: context.botToken,
+            channel: event.channel,
+            thread_ts: event.ts,
+            text: text
         });
-    
-        llmText = groundXResponse.data.search.text
-    
-        // Send GroundX response to ChatGPT API
-        const openAIResponse = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            messages: [
-                    {
-                        "role": "system",
-                        "content": `You're a helpful chatbot for an online course. Use the data below to generate a response. Indicate the sources you've been given, if any. If the provided content is inadequate to respond, answer 'I don't have enough information to answer your question.'.
-                            ===
-                            ${llmText}
-                            ===
-                            `
-                    },
-                    { 
-                        "role": "user", 
-                        "content": message 
-                    },
-            ],
-        })
-    
-        // Send ChatGPT response back to the user in the Slack thread
-            await app.client.chat.postMessage({
-                token: context.botToken,
-                channel: event.channel,
-                thread_ts: event.ts,
-                text: `Hi, <@${event.user}>! ` + openAIResponse.choices[0].message.content
-            });  
-    
+
     } catch (error) {
         console.error(error);
     }
@@ -67,6 +65,6 @@ app.event('app_mention', async ({ event, context }) => {
 });
 
 (async () => {
-    await app.start(process.env.PORT || 3000);
+    await app.start(process.env.PORT || 3001);
     console.log('⚡️ Bolt app is running!');
 })();
